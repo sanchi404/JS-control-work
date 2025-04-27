19d16f30381d75722e9483b225b7dc31950e47d5
sanchi404 <mukhanov.n95@gmail.com>
2025-04-27 19:23:41 +0200

    - finish sort value

--- main.js
+++ main.js
@@ -30,30 +30,79 @@ addBtn.addEventListener('click', () => {
         return;
     }
 
-
-    const option = document.createElement('option');
-    option.textContent = name + '=' + value;
-    pairList.appendChild(option);
+    //  создаём <li>
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    //  собираем все вместе: сначала чекбокс, потом текст
+    li.appendChild(checkbox);
+    li.append(name + '=' + value);
+    //  добавляем в наш <ul id="pairList">
+    pairList.appendChild(li);
+    //очищаем строку
     pairInput.value = '';
+    // Возращем обратно фокус после того как нажали на "Enter" или кнопку "Add"
+    pairInput.focus();
 })
 
+    //  Кнопка "Enter"
 
+pairInput.addEventListener('keydown', (ev) => {
+    if (ev.key === 'Enter') {
+        addBtn.click();
+    }
+});
 
-
-
-
-
-
-
-
-
+// сортируем по имени
 
 sortNameBtn.addEventListener('click', () => {
-    console.log("sort name clicked");
-})
+    console.log(pairList);
+
+
+    const nodeListName = document.querySelectorAll('#pairList li');
+    const massName = Array.from(nodeListName);
+    console.log(massName);
+
+    massName.sort((a, b) => {
+        const nameA = a.textContent.split('=')[0];
+        const nameB = b.textContent.split('=')[0];
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+    });
+
+    pairList.innerHTML = '';
+    console.log(massName);
+    massName.forEach(value => pairList.appendChild(value));
+});
+
+// Сортируем по Value
+
 sortValueBtn.addEventListener('click', () => {
-    console.log("sort value clicked");
+
+    const nodeListValue = document.querySelectorAll('#pairList li');
+    const massValue = Array.from(nodeListValue);
+
+    massValue.sort((a, b) => {
+        const nameA = a.textContent.split('=')[1];
+        const nameB = b.textContent.split('=')[1];
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+    })
+    pairList.innerHTML = '';
+    console.log(massValue);
+    massValue.forEach(value => pairList.appendChild(value));
+
 })
 deleteBtn.addEventListener('click', () => {
-    console.log("delete button clicked");
+
+    const items = document.querySelectorAll('#pairList li');
+
+    items.forEach(li=>{
+        const chkbox = li.querySelector('input[type="checkbox"]');
+
+    })
+
 })
+
